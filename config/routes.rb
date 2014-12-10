e6d0cc148ba1617c04fc83f9ec772ed67834c665
Kiko <mmhabib11@gmail.com>
2014-12-10 15:39:15 -0500

    Make root url /home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  root :to => 'users#index'
+  root :to => 'games#index'
   resources :user_sessions
   resources :users
   resources :games
+
+  get 'home' => 'games#index', :as => :home
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
