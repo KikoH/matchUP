70dbbf14536b7821eb393f452578c78afba0c0c5
Kiko <mmhabib11@gmail.com>
2015-07-26 15:38:30 -0400

    Carrierwave s3 image uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,14 @@
 CarrierWave.configure do |config|
+
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAI2M7HJREDVRFI26Q',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_secret_access_key  => 'Gfuv3v7S3ZpHCWTqG/rKr7pTp3o97ls8aPag9jwn',                     # required
     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
     :host                   => 's3.example.com',             # optional, defaults to nil
     :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'saragallery'                     # required
+  config.fog_directory  = 'kikomatchup'                             # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
