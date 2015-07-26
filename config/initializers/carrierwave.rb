6bc6195c5234547cb377c7c0e61b594ef99e0c4f
Kiko <mmhabib11@gmail.com>
2015-07-26 15:45:46 -0400

    Carrierwave s3 image uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,15 @@
 CarrierWave.configure do |config|
+	config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAI2M7HJREDVRFI26Q',                        # required
     :aws_secret_access_key  => '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',                     # required
     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-  }
+    :host                   => nil,             # optional, defaults to nil
+    :endpoint               => nil # optional, defaults to nil
+}
   config.fog_directory  = 'kikomatchup'                             # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
